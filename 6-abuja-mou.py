6d795dad6884bf0d021778e9ad3d7e109ef18ad5
abelsonlive <brianabelson@gmail.com>
2015-02-10 20:32:10 -0500

    updates

--- 6-abuja-mou.py
+++ 6-abuja-mou.py
@@ -7,8 +7,8 @@ output_file = 'abuja-mou-data.csv'
 url = 'http://abuja.marinet.ru/public.php/?action=getinsppublicall'
 
 form = {
-    'From': '01.01.2000',
-    'To': '30.12.2014',
+    'From': '01.01.2014',
+    'To': '11.02.2015',
     'auth': '0',
     'flag': '0',
     'ShipClass': '0',
@@ -30,8 +30,8 @@ raw_rows.extend(soup.find_all('tr', {'class': 'odd'}))
 
 data = []
 for raw_row in raw_rows:
+    print raw_row
     clean_row = [] 
-    idx = 0
     for cell in raw_row.find_all('td'):
         value = cell.text.strip()
         if value:
@@ -39,6 +39,7 @@ for raw_row in raw_rows:
     data.append(clean_row)
 
 print "WRITING {} to file".format(output_file)
+
 headers = [ 
     "date_of_inspection", "place_of_inspection", "name", 
     "call_sign", "flag", "imo", "deficiencies", "detained"
